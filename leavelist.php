643bcbd7c34e88eb92730db39069ec5b9012558d
Akash Palve <45430702+AkashPalve@users.noreply.github.com>
2021-09-12 22:44:08 +0530

    Add files via upload
    done UI changes

--- leavelist.php
+++ leavelist.php
@@ -2,47 +2,74 @@
 <head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
+<style>
+body{
+  margin:0;
+  font-family:sans-serif;
+}
+
+*{
+  box-sizing:border-box;
+}
+
+.table{
+  width:90%;
+  border-collapse:collapse;
+  padding:auto;
+  margin:auto;
+}
+.table td, .table th{
+  padding:12px 15px;
+  border:1px solid #ddd;
+  text-align:center;
+  font-size:16px;
+}
+
+.table th{
+  background-color:darkblue;
+  color:#ffffff;
+}
+
+.table tbody tr:nth-child(even){
+  background-color: #f5f5f5;
+}
+  </style>
+
 </head>
 <body>
 <?php
-//echo "Hello world";
 
-
+global $wpdb;
     
- global $wpdb;
-    
- $result = $wpdb->get_results("SELECT * FROM applyleave");
+ $result = $wpdb->get_results("SELECT * FROM leaveapply");
  ?>
-<div class="container" style="padding-top: 5em;"> 
-<table class="table table-condensed" id="table">
- <thead class="heading">
+    <br><br>  
+<table class="table" id="table">
+ <thead>
    <tr>
-    
-     <th>Employee ID</th>
+
+    <th>Employee ID</th>
      <th>Name</th>
      <th>Status</th>
+
    </tr>
    </thead>
 
 
 
-   
-
-
-
-   <?php
+ <?php
 
  foreach ($result as $row) {
      
  ?>
 
-
+<tbody>
 <tr>
    <td><?php  echo $row->emp_id;?></td>
    <td><?php  echo $row->name;?></td>
    <td><?php  echo $row->status;?></td>
  </tr>
- 
+ </tbody> 
   
 <?php
  }
@@ -51,9 +78,9 @@
 
  </table>
  <h5><b>Note<b></h5>
- <p> status 0: Reject</p>
- <p> status 1: Accept</p>
-</div>
+ <p> Status 0: Rejected</p>
+ <p> Status 1: Accepted</p>
+ 
 </body>
 
  </html>
